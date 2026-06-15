70c0b4e42470954ef71df41a71e06cdca2ec4a1b
Usernameissojol <Usernameissojol@github.com>
2026-06-15 07:19:17 +0600

    chore: update environment variables to use 127.0.0.1 for production MySQL connection

--- drivestudio.sql
+++ drivestudio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2026 at 07:05 PM
+-- Generation Time: Jun 15, 2026 at 03:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
